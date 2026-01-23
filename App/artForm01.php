cf9aca568fa7bfb69e83baf572b93d3b0c74251d
AinhoaGV <ainhoagomez@gmail.com>
2026-01-23 19:28:43 +0100

    artForm01 validación campos PHP

---
+++ App/artForm01.php
@@ -0,0 +1,87 @@
+<?php
+// aquí voy a gestionar lo que reciba del formulario
+
+// 1 recibir los datos del formulario a través de POST y los value en nuevas variables que usaré aquí
+// Comprobación de términos
+if(empty($_POST["terminos"])){
+    // como viene vacía, redirijo a la página de contacto
+    // echo "Hay un error pues no ha aceptado las condiciones de privacidad";
+    header('location:/?error=condiciones');
+    die;
+}else{
+    $terminos = $_POST["terminos"];
+}
+
+// Comprobación de Captcha
+$respUser = $_POST["respUser"];
+$respSystem = $_POST["respSystem"];
+// Vacio
+if(!isset($respUser)){
+    header('location:/?error=captchaVacio');
+    die;
+}
+// No coinciden
+if($respUser != $respSystem){
+    header('location:/?error=captchaError');
+    die;
+}
+// 2 comprobar que los datos son correctos
+$nombre = $_POST["nombre"];
+$telefono = $_POST["telefono"];
+$email = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+
+//Si nombre viene vacio
+if(empty($nombre)){
+    header('location:/?error=nombreVacio');
+    die;
+}
+// Si nombre es menor de 3 o mayor de 40
+$contadorCaracteres = strlen($nombre);
+if($contadorCaracteres<3 || $contadorCaracteres>40){
+    header('location:/?error=nombreCaracteres');
+    die;
+}
+// Si teléfono viene vacio
+if(empty($telefono)){
+    header('location:/?error=telefonoVacio');
+    die;
+}
+
+// Si el email viene vacio
+if(empty($email)){
+    header('location:/?error=emailVacio');
+    die;
+}
+
+//Expresión regular para comprobar formato email
+$patron = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+if (!preg_match($patron, $email)) {
+    header('location:/?error=emailFormato');
+    die;
+}
+// if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//     header('location:/?error=emailFormato');
+//     die;
+// }
+
+// SI el mensaje viene vacio
+if(empty($mensaje)){
+    header('location:/?error=mensajeVacio');
+    die;
+}
+// Si nombre es menor de 4 o mayor de 200
+$contadorCaracteres = strlen($mensaje);
+if($contadorCaracteres<5 || $contadorCaracteres>200){
+    header('location:/?error=mensajeCaracteres');
+    die;
+}
+
+// 3 Enviar emails
+
+// 4 guardar los datos en una base de datos
+
+// 5 enviar correos de aviso: a la empresa y al propio usuario
+
+// 6  redirigir a la página de gracias
+?>
---
+++ src/resources/_artForm01.js
@@ -0,0 +1,38 @@
+// OBJETIVOS
+// RECOJO EN CONSTANTES LOS ELEMENTOS A LOS QUE TENDRÉ QUE INSERTAR LOS NÚMEROS RANDOM Y TAMBIÉN EL INPUT
+const num1 = document.getElementById("num1")
+const num2 = document.getElementById("num2")
+const operador = document.getElementById("operador")
+const respSystem = document.getElementById("respSystem")
+// CALCULAR DOS NÚMEROS RANDOM DEL 0 AL 10
+let valorNum1 = (Number)(Math.floor(Math.random()*10))
+let valorNum2 = (Number)(Math.floor(Math.random()*10))
+
+// EJERCICIO EXTRA: MÁXIMO 4 CON SWITCH CASE
+let valorNum3 = (Number)(Math.floor(Math.random()*3))
+let resultado
+switch (valorNum3){
+    case 0:
+        resultado = valorNum1 + valorNum2
+        operador.innerText = "+"
+        break
+    case 1:
+        resultado = valorNum1 - valorNum2
+        operador.innerText = "-"
+        break
+    case 2:
+        resultado = valorNum1 * valorNum2
+        operador.innerText = "x"
+        break
+    default:
+        console.log("No está dentro")
+        break
+}
+
+// HACER SU SUMA
+//resultado = valorNum1 + valorNum2
+// INSERTARLOS COMO TEXTOS DENTRO DE DOS SPAN HTML
+num1.innerText = valorNum1
+num2.innerText = valorNum2
+// EL RESULTADO DE SU SUMA, INSERTARLO COMO VALUE DENTRO DEL INPUT OCULTO
+respSystem.value = resultado
