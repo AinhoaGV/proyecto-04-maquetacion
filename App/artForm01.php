652e0d72bfb2c29a4d7592c001986268d8924aed
AinhoaGV <ainhoagomez@gmail.com>
2026-01-26 19:31:38 +0100

    Crear funciones en helper.php

--- App/artForm01.php
+++ App/artForm01.php
@@ -1,16 +1,24 @@
 <?php
+//funciones externas
+include_once("../config/helper.php");
 // aquí voy a gestionar lo que reciba del formulario
 
 // 1 recibir los datos del formulario a través de POST y los value en nuevas variables que usaré aquí
 // Comprobación de términos
-if(empty($_POST["terminos"])){
-    // como viene vacía, redirijo a la página de contacto
-    // echo "Hay un error pues no ha aceptado las condiciones de privacidad";
+if(comprobarVacio($_POST["terminos"])){
     header('location:/?error=condiciones');
     die;
 }else{
     $terminos = $_POST["terminos"];
 }
+// if(empty($_POST["terminos"])){
+//     // como viene vacía, redirijo a la página de contacto
+//     // echo "Hay un error pues no ha aceptado las condiciones de privacidad";
+//     header('location:/?error=condiciones');
+//     die;
+// }else{
+//     $terminos = $_POST["terminos"];
+// }
 
 // Comprobación de Captcha
 $respUser = $_POST["respUser"];
@@ -32,50 +40,62 @@ $email = $_POST["email"];
 $mensaje = $_POST["mensaje"];
 
 //Si nombre viene vacio
-if(empty($nombre)){
+if(comprobarVacio($nombre)){
     header('location:/?error=nombreVacio');
     die;
 }
 // Si nombre es menor de 3 o mayor de 40
-$contadorCaracteres = strlen($nombre);
-if($contadorCaracteres<3 || $contadorCaracteres>40){
+if(comprobarCaracteres($nombre, 3, 40)){
     header('location:/?error=nombreCaracteres');
     die;
 }
+// $contadorCaracteres = strlen($nombre);
+// if($contadorCaracteres<3 || $contadorCaracteres>40){
+//     header('location:/?error=nombreCaracteres');
+//     die;
+// }
 // Si teléfono viene vacio
-if(empty($telefono)){
+if(comprobarVacio($telefono)){
     header('location:/?error=telefonoVacio');
     die;
 }
 
 // Si el email viene vacio
-if(empty($email)){
+if(comprobarVacio($email)){
     header('location:/?error=emailVacio');
     die;
 }
 
 //Expresión regular para comprobar formato email
-$patron = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
-if (!preg_match($patron, $email)) {
+if (!comprobarEmail($email)) {
     header('location:/?error=emailFormato');
     die;
 }
+// $patron = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+// if (!preg_match($patron, $email)) {
+//     header('location:/?error=emailFormato');
+//     die;
+// }
 // if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 //     header('location:/?error=emailFormato');
 //     die;
 // }
 
 // SI el mensaje viene vacio
-if(empty($mensaje)){
+if(comprobarVacio($mensaje)){
     header('location:/?error=mensajeVacio');
     die;
 }
 // Si nombre es menor de 4 o mayor de 200
-$contadorCaracteres = strlen($mensaje);
-if($contadorCaracteres<5 || $contadorCaracteres>200){
+if(comprobarCaracteres($mensaje, 5, 200)){
     header('location:/?error=mensajeCaracteres');
-    die;
+    die;    
 }
+// $contadorCaracteres = strlen($mensaje);
+// if($contadorCaracteres<5 || $contadorCaracteres>200){
+//     header('location:/?error=mensajeCaracteres');
+//     die;
+// }
 
 // 3 Enviar emails
 
@@ -84,4 +104,7 @@ if($contadorCaracteres<5 || $contadorCaracteres>200){
 // 5 enviar correos de aviso: a la empresa y al propio usuario
 
 // 6  redirigir a la página de gracias
+$nombreURL = urlencode($nombre);
+header("location:/gracias.php?nom=$nombreURL");
+die;
 ?>
---
+++ config/helper.php
@@ -0,0 +1,24 @@
+<?php
+//recibir el valor del parámetro y comproba si es vacio o no, y devolver true si est vacío o y falde en caso contrario
+function comprobarVacio($param1){
+    if(empty($param1)){
+        return true;
+    }else{
+        return false;
+    }
+}
+// Una función para comparar si es mayor que un valor y menor que otro valor y devolver false si no cumple esa condición y true si la cumple
+function comprobarCaracteres($campo, $min, $max){
+    $caracteres = strlen($campo);
+    if($caracteres<$min || $caracteres>$max){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// función para comprobar si la estructura del correo recibido es acorde a la expresión regulasr. En el caso de que sea diferente devolveremos false y si es correcta devolvemos true.
+function comprobarEmail($email){
+    $patron = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+    return preg_match($patron, $email);
+}
